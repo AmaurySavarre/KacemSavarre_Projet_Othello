424c01839d7caadd70f74feb6c67fd4f76caed3b
AmaurySavarre <amaury.savarre@gmail.com>
2016-01-26 20:40:13 +0100

    Ménage et modification du layout du menu principal.

--- app/src/main/java/com/example/utilisateur/othello/MainMenuActivity.java
+++ app/src/main/java/com/example/utilisateur/othello/MainMenuActivity.java
@@ -2,7 +2,7 @@ package com.example.utilisateur.othello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -14,7 +14,28 @@ public class MainMenuActivity extends AppCompatActivity {
 
     public void onPlay(View v)
     {
-        Intent intent = new Intent (MainMenuActivity.this , VersusActivity.class);
-        startActivity (intent);
+        /*Intent intent = new Intent (MainMenuActivity.this , VersusActivity.class);
+        startActivity (intent);*/
+        Toast.makeText(getApplicationContext(), "onPlay()", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onHighScores(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onHighScores()", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onExit(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onExit()", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onRules(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onRules()", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSettings(View v)
+    {
+        Toast.makeText(getApplicationContext(), "onSettings()", Toast.LENGTH_SHORT).show();
     }
 }
