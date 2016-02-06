8c3e29283cd5b5c2d2f45f478d6d6be8f8d2ab1f
rimakacem <rim.kacem93@gmail.com>
2016-02-06 12:04:18 +0100

    Ajout du splash screen.

---
+++ app/src/main/java/com/example/utilisateur/othello/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package com.example.utilisateur.othello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen1);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        RelativeLayout l = (RelativeLayout) findViewById(R.id.splash_layout1);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        RelativeLayout l2 = (RelativeLayout) findViewById(R.id.splash_layout2);
+        l2.setVisibility(View.VISIBLE);
+        l2.clearAnimation();
+        l2.startAnimation(anim);
+    }
+}
